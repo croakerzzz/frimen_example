601afaf7e53084f897705059b84ca376fd696a60
Sergey Elshin <elshin@proitr.ru>
2023-08-11 12:42:48 +0500

    15.15

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,7 +1,7 @@
 import {
     Directive,
     ElementRef,
-    EventEmitter,
+    EventEmitter, HostBinding, HostListener,
     Input,
     OnChanges,
     OnInit,
@@ -16,15 +16,16 @@ import {Product} from "./model/product.model";
 })
 export class PaAttrDirective implements OnInit, OnChanges {
 
-    constructor(private element: ElementRef) {
-        this.element.nativeElement.addEventListener("click", () => {
-            if (this.product != null) {
-                this.click.emit(this.product.category);
-            }
-        });
-    }
+    // constructor(private element: ElementRef) {
+    //     this.element.nativeElement.addEventListener("click", () => {
+    //         if (this.product != null) {
+    //             this.click.emit(this.product.category);
+    //         }
+    //     });
+    // }
 
     @Input("pa-attr")
+    @HostBinding("class")
     bgClass?: string | null;
 
     @Input("pa-product")
@@ -33,21 +34,28 @@ export class PaAttrDirective implements OnInit, OnChanges {
     @Output("pa-category")
     click = new EventEmitter<string>();
 
+    @HostListener("click")
+    triggerCustomEvent() {
+        if (this.product != null) {
+            this.click.emit(this.product.category);
+        }
+    }
+
     ngOnInit() {
-        this.element.nativeElement.classList.add(this.bgClass || "bg-success");
+        // this.element.nativeElement.classList.add(this.bgClass || "bg-success");
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        console.log(changes);
-
-        let change = changes["bgClass"];
-        let classList = this.element.nativeElement.classList;
-        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-            classList.remove(change.previousValue);
-        }
-        if (!classList.contains(change.currentValue)) {
-            classList.add(change.currentValue);
-        }
+        // console.log(changes);
+        //
+        // let change = changes["bgClass"];
+        // let classList = this.element.nativeElement.classList;
+        // if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+        //     classList.remove(change.previousValue);
+        // }
+        // if (!classList.contains(change.currentValue)) {
+        //     classList.add(change.currentValue);
+        // }
     }
 
 }
