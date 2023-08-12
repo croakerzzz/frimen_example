a97662073019a1d7b7d987cd068da1137f9c60f9
Sergey Elshin <elshin@proitr.ru>
2023-08-12 11:51:17 +0500

    17.15

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {ProductFormGroup} from "../form.model";
+import {Product} from "../model/product.model";
 
 @Component({
-  selector: 'pa-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+    selector: 'pa-product-form',
+    templateUrl: './product-form.component.html',
+    styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent {
+
+    form: ProductFormGroup = new ProductFormGroup();
+
+    newProduct: Product = new Product();
+
+    formSubmitted: boolean = false;
+
+    @Output("paNewProduct")
+    newProductEvent = new EventEmitter<Product>();
+
+    submitForm(form: any) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.newProductEvent.emit(this.newProduct);
+            this.newProduct = new Product();
+            this.form.reset();
+            this.formSubmitted = false;
+        }
+    }
+
+    key(keyName: string): keyof Product {
+        return keyName as keyof Product;
+    }
 
 }
