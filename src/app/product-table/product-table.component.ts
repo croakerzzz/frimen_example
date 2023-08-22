ad7a69a04db0ebf8428e6f18766043996a406143
munory <gluchova.ka@gmail.com>
2023-08-22 17:17:17 +0500

    18.23

--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -11,6 +11,10 @@ export class ProductTableComponent {
 
     taxRate = "0";
 
+    dateObject: Date = new Date(2020, 1, 20);
+    dateString: string = "2020-02-20T00:00:00.000Z";
+    dateNumber: number = 1582156867500;
+
     @Input("model")
     dataModel!: Model;
     categoryFilter: any;
